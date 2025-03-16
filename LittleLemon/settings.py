7da61383a402ce2d629852310e2cdae8a983fa44
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-03-16 02:43:29 +0200

    Added models

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'LittleLemonAPI',
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
