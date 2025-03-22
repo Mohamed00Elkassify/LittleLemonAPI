dd5eb0ff6e4d0a170d839c5be8fca2f9715bb862
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-03-22 14:46:13 +0200

    register models, fix issues

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -4,15 +4,19 @@ from . models import *
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Categoryfields = ['id', 'slug', 'title']
+        model = Category
+        fields = ['id', 'slug', 'title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'featured', 'category']
 
 class CartSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  # Make user read-only
+    unit_price = serializers.DecimalField(read_only=True, max_digits=6, decimal_places=2)  # Make unit_price read-only
+    total_price = serializers.DecimalField(read_only=True, max_digits=6, decimal_places=2)  # Make total_price read-only
     class Meta:
         model = Cart
         fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'total_price']
